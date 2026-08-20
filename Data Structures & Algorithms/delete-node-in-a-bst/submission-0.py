0ff5c4c288ec3370cda2eb66b9a34609a2b352c2
Anil Kumar Raut <anilkumarraut11@gmail.com>
2026-08-20 19:20:53 -0400

    Update: delete-node-in-a-bst - submission-0

--- Data Structures & Algorithms/delete-node-in-a-bst/submission-0.py
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.py
@@ -8,20 +8,51 @@ class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return root
-        if key > root.val:
-            root.right = self.deleteNode(root.right,key)
-        elif key < root.val:
-            root.left = self.deleteNode(root.left,key)
-        else:
-            if not root.left:
-                return root.right
-            elif not root.right:
-                return root.left
-            
-            cur = root.right
-            while cur.left:
+
+        parent = None
+        cur = root
+
+        # Find the node to delete
+        while cur and cur.val != key:
+            parent = cur
+            if key > cur.val:
+                cur = cur.right
+            else:
                 cur = cur.left
-            root.val = cur.val
-            root.right = self.deleteNode(root.right, root.val)
+
+        if not cur:
+            return root
+
+        # Node with only one child or no child
+        if not cur.left or not cur.right:
+            child = cur.left if cur.left else cur.right
+            if not parent:
+                return child
+            if parent.left == cur:
+                parent.left = child
+            else:
+                parent.right = child
+        else:
+            # Node with two children
+            par = None  # parent of right subTree min node
+            delNode = cur
+            cur = cur.right
+            while cur.left:
+                par = cur
+                cur = cur.left
+
+            if par:  # if there was a left traversal
+                par.left = cur.right
+                cur.right = delNode.right
+
+            cur.left = delNode.left
+
+            if not parent:  # if we're deleting root
+                return cur
+
+            if parent.left == delNode:
+                parent.left = cur
+            else:
+                parent.right = cur
 
         return root
