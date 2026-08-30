000f5b8b029f2060cd961a9331d6221aa73680f5
Anil Kumar Raut <anilkumarraut11@gmail.com>
2026-08-30 12:43:41 -0400

    Update: binary-search - submission-1

--- Data Structures & Algorithms/binary-search/submission-1.py
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -1,12 +1,14 @@
 class Solution:
-    def binarySearch(self, l, r, nums, target):
+    def binary_search(self, l: int, r: int, nums: List[int], target: int) -> int:
         if l > r:
             return -1
-        m = (l+r)//2
+        m = l + (r - l) // 2
+
         if nums[m] == target:
             return m
-        if nums[m] > target:
-            return self.binarySearch(l, m-1, nums, target)
-        return self.binarySearch(m+1, r, nums, target)
+        if nums[m] < target:
+            return self.binary_search(m + 1, r, nums, target)
+        return self.binary_search(l, m - 1, nums, target)
+
     def search(self, nums: List[int], target: int) -> int:
-        return self.binarySearch(0,len(nums)-1,nums,target)
+        return self.binary_search(0, len(nums) - 1, nums, target)
